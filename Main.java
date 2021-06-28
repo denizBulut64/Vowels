d1fd6581c1602e21fdc09a5ee8c93a7ffe9a3669
denizBulut64 <86578400+denizBulut64@users.noreply.github.com>
2021-06-28 03:39:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char harf = 'E';
+
+
+        switch (harf) {
+
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+
+                System.out.println("Harf kalin seslidir.");
+                break;
+            default:
+                System.out.println("Harf ince seslidir.");
+
+
+        }
+
+
+    }
+
+
+}
+
